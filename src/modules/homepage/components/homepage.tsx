e1c1f552fe360b7a82050faa9a4a6b5986449b68
mphomolefe730 <mphomolefe730@gmail.com>
2025-03-22 00:55:51 +0200

    add animation for welcome page using switch statement and making the main components disappear

--- src/modules/homepage/components/homepage.tsx
+++ src/modules/homepage/components/homepage.tsx
@@ -1,26 +1,52 @@
 import './homepage.css';
+import { useState, useEffect } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 
 function HomePage(){
+	let [ welcomeMessage, setWelcomeMessage ] = useState('WELCOME');
+	const [visible, setVisible] = useState(1);
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			switch(visible){
+				case 1:
+					setWelcomeMessage('UYAKWAMUKELA');
+					break;
+				case 2:
+					setWelcomeMessage('WELKOM');
+					break;
+				case 3:
+					setWelcomeMessage('AMOHELANG');
+					break;
+				default:
+					setWelcomeMessage('WELCOME');
+					break;
+			}
+			if(visible < 5){
+				setVisible(visible+1);
+			}
+		}, 500);
+		return () => clearTimeout(timer);
+	},[visible]);
 
 	return(
 		<div style={{ height: '89svh', display:'grid' , gridTemplateRows: '2fr 4fr'}}>
-			<Carousel fade className='carouselHolder'>
+			<h1 style={{ display: (visible < 5)? 'flex' : 'none'}} className='welcomeText' >{ welcomeMessage }</h1>
+			<Carousel fade className='carouselHolder' style={{ display: (visible == 5) ? 'block' : 'none', animation: 'fadeAnimation 2s' }}>
 				<Carousel.Item>
 					<img className="carouselImage" src="https://raw.githubusercontent.com/mphomolefe730/portfolio-website/refs/heads/main/src/assets/background_images/53232095831_d69beeb347_k.jpg" alt="First slide"/>
-					<Carousel.Caption style={{backgroundColor: "rgba(0,0,0,0.25)"}}>
+					<Carousel.Caption>
 						<p>Geekulture Unisa Hackathon 2023</p>
 					</Carousel.Caption>
 				</Carousel.Item>
 				<Carousel.Item>
 					<img className="carouselImage" src="https://raw.githubusercontent.com/mphomolefe730/portfolio-website/refs/heads/main/src/assets/background_images/WhatsApp Image 2023-10-01 at 08.53.24.jpeg" alt="Second slide"/>
-					<Carousel.Caption style={{backgroundColor: "rgba(0,0,0,0.25)"}}>
+					<Carousel.Caption>
 						<p>Geekulture Unisa Hackathon 2023</p>
 					</Carousel.Caption>
 				</Carousel.Item>
 				<Carousel.Item>
 					<img className="carouselImage" src="https://raw.githubusercontent.com/mphomolefe730/portfolio-website/refs/heads/main/src/assets/background_images/WhatsApp Image 2024-08-29 at 9.23.45 PM.jpeg" alt="Third slide"/>
-					<Carousel.Caption style={{backgroundColor: "rgba(0,0,0,0.25)"}}>
+					<Carousel.Caption>
 						<p>AWS Summit Johannesburg 2024</p>
 					</Carousel.Caption>
 				</Carousel.Item>
@@ -31,6 +57,7 @@ function HomePage(){
 				<p><a style={{ color: "white" }} href="https://x.com/mphomolefeww">Twitter</a></p>
 			</div>
 			*/}
+
 			<div className='mainContainer'>
 				<div className='elements'>
 					<h1 style={{margin:'0'}}>MPHO MOLEFE</h1>
