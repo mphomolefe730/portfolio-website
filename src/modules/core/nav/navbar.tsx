a4e3af35b3e2d146e4550e557eaba6da0bc4a42e
mphomolefe730 <mphomolefe730@gmail.com>
2025-05-28 01:05:20 +0200

    add social media access to website

--- src/modules/core/nav/navbar.tsx
+++ src/modules/core/nav/navbar.tsx
@@ -4,6 +4,18 @@ import { useState } from 'react';
 
 function NavBar(){
 	let [open, setOpen] = useState(false);
+	let socials: {name:string,link:string}[] = [
+		{
+			name: "LinkedIn",
+			link: "http://linkedin.com/in/mpho-molefe-a67ab9284/"
+		},{
+			name: "github",
+			link: "https://github.com/mphomolefe730"
+		},{
+			name: "email",
+			link: "mailto:mphomolefe730@gmail.com"
+		}
+	]
 	let showNav = () => {
 		setOpen(!open);
 	}
@@ -13,6 +25,15 @@ function NavBar(){
 			<a className='mm desktopNav' href='/'> 
 				MM
 			</a>
+			<div className='socialContainer desktopNav'>
+				{
+					socials.map((socialMedia,index) => {
+						return(
+							<a className='informationPill' target="_blank" href={socialMedia.link}> {socialMedia.name} </a>
+						)
+					})
+				}
+			</div>
 			<div className="desktopNav">
 				<div className='navContainer'>
 					<a className='navTitle' href='/about'>About</a>
@@ -29,7 +50,17 @@ function NavBar(){
 					<img style={{ maxWidth: "50px", backgroundColor: "white", padding: "10px", borderRadius: "50%" }} src="https://raw.githubusercontent.com/mphomolefe730/portfolio-website/refs/heads/main/src/assets/icons/menu-burger-horizontal-svgrepo-com.svg" onClick={()=> showNav()}/>
 				</div>
 				<div style={{ display: (open) ? "grid": "none", height: "98svh", width: "100%", zIndex: 99, position: "absolute", gridTemplateColumns: "2fr 4fr"}}>
-					<div style={{ backgroundColor: "rgba(0,0,0,0.5)" }} onClick={()=> showNav()}></div>
+					<div style={{ backgroundColor: "rgba(0,0,0,0.5)" }} onClick={()=> showNav()}>
+						<div className='socialContainer '>
+							{
+								socials.map((socialMedia,index) => {
+									return(
+										<a className='informationPill' target="_blank" href={socialMedia.link}> {socialMedia.name} </a>
+									)
+								})
+							}
+						</div>
+					</div>
 					<div style={{ backgroundColor: "rgba(255,255,255,1)", padding: "10px"}}>
 						<a className='navTitle' href='/about'><p>About</p></a>
 						<a className='navTitle' href='/projects'><p>Projects</p></a>
